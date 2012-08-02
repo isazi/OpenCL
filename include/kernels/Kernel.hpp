9f528acb010948ad65717f3a8701986a4228b1d8
Alessio Sclocco <a.sclocco@vu.nl>
2012-08-02 12:10:26 +0200

    Begin with the refactoring of Kernel. I am aiming at a better interface.

--- include/kernels/Kernel.hpp
+++ include/kernels/Kernel.hpp
@@ -20,17 +20,19 @@
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 #include <string>
-using std::string;
 #include <utility>
+
+using std::string;
 using std::make_pair;
 
 #include <Timer.hpp>
-using LOFAR::NSTimer;
 #include <Exceptions.hpp>
-using isa::Exceptions::OpenCLError;
 #include <GPUData.hpp>
-using isa::OpenCL::GPUData;
 #include <utils.hpp>
+
+using LOFAR::NSTimer;
+using isa::Exceptions::OpenCLError;
+using isa::OpenCL::GPUData;
 using isa::utils::toStringValue;
 
 
@@ -43,51 +45,65 @@ namespace OpenCL {
 template< typename T > class Kernel {
 public:
 	Kernel(string name, string dataType);
-	~Kernel();
-	
-	void compile(cl::Context &clContext, cl::Device &clDevice, cl::CommandQueue *queue, string &code) throw (OpenCLError);
-	template< typename A > inline void setArgument(unsigned int id, A param) throw (OpenCLError);
-	void run(cl::NDRange &globalSize, cl::NDRange &localSize) throw (OpenCLError);
+	virtual ~Kernel();
 
+	virtual void generateCode() = 0;
+	inline void bindOpenCL(cl::Context *context, cl::Device *device, cl::CommandQueue *queue);
 	inline void setAsync(bool asy);
 
 	inline string getName() const;
+	inline string getCode() const;
 	inline string getDataType() const;
 	inline string getBuildLog() const;
 	inline double getTime() const;
+	inline double getArithmeticIntensity() const;
+	inline double getGFLOP() const;
+	inline double getGB() const;
 
-private:
+protected:
+	void compile() throw (OpenCLError);
+	template< typename A > inline void setArgument(unsigned int id, A param) throw (OpenCLError);
+	void run(cl::NDRange &globalSize, cl::NDRange &localSize) throw (OpenCLError);
+
 	bool async;
 	string name;
+	string *code;
 	string dataType;
 	string buildLog;
 	cl::Kernel *kernel;
+	cl::Context *clContext;
+	cl::Device *clDevice;
 	cl::CommandQueue *clCommands;
 	cl::Event clEvent;
 	NSTimer timer;
+	
+	double arInt;
+	double gflop;
+	double gb;
 };
 
 
 // Implementation
 
-template< typename T > Kernel< T >::Kernel(string name, string dataType) : async(false), name(name), dataType(dataType), buildLog(string()), kernel(0), clCommands(0), clEvent(cl::Event()), timer(NSTimer(name, false, false)) {}
+template< typename T > Kernel< T >::Kernel(string name, string dataType) : async(false), name(name), code(0), dataType(dataType), buildLog(string()), kernel(0), clContext(0), clDevice(0), clCommands(0), clEvent(cl::Event()), timer(NSTimer(name, false, false)), arInt(0.0), gflop(0.0), gb(0.0) {}
 
 
 template< typename T > Kernel< T >::~Kernel() {
+	if ( code != 0 ) {
+		delete code;
+	}
 	if ( kernel != 0 ) {
 		delete kernel;
 	}
 }
 
 
-template< typename T > void Kernel< T >::compile(cl::Context &clContext, cl::Device &clDevice, cl::CommandQueue *queue, string &code) throw (OpenCLError) {
-	clCommands = queue;
-
+template< typename T > void Kernel< T >::compile() throw (OpenCLError) {
 	cl::Program *program;
 	try {
 		cl::Program::Sources sources(1, make_pair(code.c_str(), code.length()));
-		program = new cl::Program(clContext, sources, NULL);
-		program->build(vector< cl::Device >(1, clDevice), "-cl-mad-enable", NULL, NULL);
+		program = new cl::Program(*clContext, sources, NULL);
+		program->build(vector< cl::Device >(1, *clDevice), "-cl-mad-enable", NULL, NULL);
 		buildLog = program->getBuildInfo< CL_PROGRAM_BUILD_LOG >(clDevice);
 	}
 	catch ( cl::Error err ) {	
@@ -150,6 +166,13 @@ template< typename T > void Kernel< T >::run(cl::NDRange &globalSize, cl::NDRang
 	}
 }
 
+	
+template< typename T > inline void Kernel< T >::bindOpenCL(cl::Context *context, cl::Device *device, cl::CommandQueue *queue) {
+	clContext = context;
+	clDevice = device;
+	clCommands = queue;
+}
+
 
 template< typename T > inline void Kernel< T >::setAsync(bool asy) {
 	async = asy;
@@ -158,6 +181,15 @@ template< typename T > inline void Kernel< T >::setAsync(bool asy) {
 
 template< typename T > inline string Kernel< T >::getName() const {
 	return name;
+}
+
+
+template< typename T > inline string Kernel< T >::getCode() const {
+	if ( code != 0 ) {
+		return *code;
+	}
+	
+	return string();
 }
 
 
@@ -173,6 +205,21 @@ template< typename T > inline string Kernel< T >::getBuildLog() const {
 
 template< typename T > inline double Kernel< T >::getTime() const {
 	return timer.getElapsed();
+}
+
+
+template< typename T > inline double Kernel< T >::getArithmeticIntensity() const {
+	return arInt;
+}
+
+
+template< typename T > inline double Kernel< T >::getGFLOP() const {
+	return gflop;
+}
+
+
+template< typename T > inline double Kernel< T >::getGB() const {
+	return gb;
 }
 
 } // OpenCL
--- include/kernels/VectorAdd.hpp
+++ include/kernels/VectorAdd.hpp
@@ -20,17 +20,19 @@
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 #include <string>
-using std::string;
 #include <utility>
+
+using std::string;
 using std::make_pair;
 
 #include <kernels/Kernel.hpp>
-using isa::OpenCL::Kernel;
 #include <GPUData.hpp>
-using isa::OpenCL::GPUData;
 #include <Exceptions.hpp>
-using isa::Exceptions::OpenCLError;
 #include <utils.hpp>
+
+using isa::OpenCL::Kernel;
+using isa::OpenCL::GPUData;
+using isa::Exceptions::OpenCLError;
 using isa::utils::giga;
 
 
@@ -44,74 +46,56 @@ namespace OpenCL {
 template < typename T > class VectorAdd : public Kernel< T > {
 public:
 	VectorAdd(string dataType);
-	~VectorAdd();
 
-	void compile(cl::Context &clContext, cl::Device &clDevice, cl::CommandQueue *clCommands) throw (OpenCLError);
-	void run(GPUData< T > *a, GPUData< T > *b, GPUData< T > *c) throw (OpenCLError);
-	
+	void operator()(GPUData< T > *a, GPUData< T > *b, GPUData< T > *c) throw (OpenCLError);
+
 	inline void setNrThreadsPerBlock(unsigned int threads);
 	inline void setNrThreads(unsigned int threads);
 	inline void setNrRows(unsigned int rows);
 
-	inline string getCode() const;
-	inline double getArithmeticIntensity() const;
-	inline double getGFLOP() const;
-	inline double getGB() const;
-
 private:
-	string *code;
 	unsigned int nrThreadsPerBlock;
 	unsigned int nrThreads;
 	unsigned int nrRows;
 
-	double arInt;
-	double gflop;
-	double gb;
 };
 
 
 // Implementation
 
-template< typename T > VectorAdd< T >::VectorAdd(string dataType) : Kernel< T >("VectorAdd", dataType), code(0), nrThreadsPerBlock(0), nrThreads(0), nrRows(0), arInt(0.0), gflop(0.0), gb(0.0) {}
+template< typename T > VectorAdd< T >::VectorAdd(string dataType) : Kernel< T >("VectorAdd", dataType), nrThreadsPerBlock(0), nrThreads(0), nrRows(0) {}
 
 
-template< typename T > VectorAdd< T >::~VectorAdd() {
-	if ( code != 0 ) {
-		delete code;
-	}
-}
-
-
-template< typename T > void VectorAdd< T >::compile(cl::Context &clContext, cl::Device &clDevice, cl::CommandQueue *clCommands) throw (OpenCLError) {
+template< typename T > virtual void VectorAdd< T >::generateCode() throw (OpenCLError) {
 	long long unsigned int ops = static_cast< long long unsigned int >(nrThreads);
 	long long unsigned int memOps = ops * 12;
 
-	arInt = ops / static_cast< double >(memOps);
-	gflop = giga(ops);
-	gb = giga(memOps);
+	this->arInt = ops / static_cast< double >(memOps);
+	this->gflop = giga(ops);
+	this->gb = giga(memOps);
 	
-	if ( code != 0 ) {
-		delete code;
+	if ( this->code != 0 ) {
+		delete this->code;
 	}
-	code = new string();
-	*code = "__kernel void " + Kernel< T >::getName() + "(__global " + Kernel< T >::getDataType() + " *A, __global " + Kernel< T >::getDataType() + " *B, __global " + Kernel< T >::getDataType() + " *C) {\n"
+	this->code = new string();
+	*(this->code) = "__kernel void " + this->name + "(__global " + this->dataType + " *A, __global " +  this->dataType + " *B, __global " + this->dataType + " *C) {\n"
 		"unsigned int id = (get_group_id(1) * get_num_groups(0) * get_local_size(0)) + (get_group_id(0) * get_local_size(0)) + get_local_id(0);\n"
 		"C[id] = A[id] + B[id];\n"
 		"}";
 
-	Kernel< T >::compile(clContext, clDevice, clCommands, *code);
+	this->compile();
 }
 
 
-template< typename T > void VectorAdd< T >::run(GPUData< T > *a, GPUData< T > *b, GPUData< T > *c) throw (OpenCLError) {
+template< typename T > void VectorAdd< T >::operator()(GPUData< T > *a, GPUData< T > *b, GPUData< T > *c) throw (OpenCLError) {
 	cl::NDRange globalSize(nrThreads / nrRows, nrRows);
 	cl::NDRange localSize(nrThreadsPerBlock, 1);
 
-	Kernel< T >::setArgument(0, *(a->getDeviceData()));
-	Kernel< T >::setArgument(1, *(b->getDeviceData()));
-	Kernel< T >::setArgument(2, *(c->getDeviceData()));
+	this->setArgument(0, *(a->getDeviceData()));
+	this->setArgument(1, *(b->getDeviceData()));
+	this->setArgument(2, *(c->getDeviceData()));
 
-	Kernel< T >::run(globalSize, localSize);
+	this->run(globalSize, localSize);
 }
 
 
@@ -127,31 +111,6 @@ template< typename T > inline void VectorAdd< T >::setNrThreads(unsigned int thr
 
 template< typename T > inline void VectorAdd< T >::setNrRows(unsigned int rows) {
 	nrRows = rows;
-}
-
-
-
-template< typename T > inline string VectorAdd< T >::getCode() const {
-	if ( code != 0 ) {
-		return *code;
-	}
-	
-	return string();
-}
-
-
-template< typename T > inline double VectorAdd< T >::getArithmeticIntensity() const {
-	return arInt;
-}
-
-
-template< typename T > inline double VectorAdd< T >::getGFLOP() const {
-	return gflop;
-}
-
-
-template< typename T > inline double VectorAdd< T >::getGB() const {
-	return gb;
 }
 
 } // OpenCL
