15a5b6cef0ecf3d5b1489d9c7c86a1b1c57f4efd
Alessio Sclocco <alessio@sclocco.eu>
2016-03-17 13:13:59 +0100

    Having a general "configuration" class that can be extended in case a kernel has more parameters.

--- include/Kernel.hpp
+++ include/Kernel.hpp
@@ -28,7 +28,84 @@
 namespace isa {
 namespace OpenCL {
 
+// Class that represents the configuration of an OpenCL kernel
+class KernelConf {
+public:
+  KernelConf();
+  ~KernelConf();
+  // Get
+  inline unsigned int getNrThreadsD0() const;
+  inline unsigned int getNrThreadsD1() const;
+  inline unsigned int getNrThreadsD2() const;
+  inline unsigned int getNrItemsD0() const;
+  inline unsigned int getNrItemsD1() const;
+  inline unsigned int getNrItemsD2() const;
+  // Set
+  inline void setNrThreadsD0(unsigned int threads);
+  inline void setNrThreadsD1(unsigned int threads);
+  inline void setNrThreadsD2(unsigned int threads);
+  inline void setNrItemsD0(unsigned int items);
+  inline void setNrItemsD1(unsigned int items);
+  inline void setNrItemsD2(unsigned int items);
+  // utils
+  std::string print() const;
+
+private:
+  unsigned int nrThreadsD0, nrThreadsD1, nrThreadsD2;
+  unsigned int nrItemsD0, nrItemsD1, nrItemsD2;
+};
+
 cl::Kernel * compile(const std::string & name, const std::string & code, const std::string & flags, cl::Context & clContext, cl::Device & clDevice) throw (isa::OpenCL::OpenCLError);
+
+
+// Implementations
+inline unsigned int KernelConf::getNrThreadsD0() const {
+  return nrThreadsD0;
+}
+
+inline unsigned int KernelConf::getNrThreadsD1() const {
+  return nrThreadsD1;
+}
+
+inline unsigned int KernelConf::getNrThreadsD2() const {
+  return nrThreadsD2;
+}
+
+inline unsigned int KernelConf::getNrItemsD0() const {
+  return nrItemsD0;
+}
+
+inline unsigned int KernelConf::getNrItemsD1() const {
+  return nrItemsD1;
+}
+
+inline unsigned int KernelConf::getNrItemsD2() const {
+  return nrItemsD2;
+}
+
+inline void KernelConf::setNrThreadsD0(unsigned int threads) {
+  nrThreadsD0 = threads;
+}
+
+inline void KernelConf::setNrThreadsD1(unsigned int threads) {
+  nrThreadD1 = threads;
+}
+
+inline void KernelConf::setNrThreadsD2(unsigned int threads) {
+  nrThreadsD2 = threads;
+}
+
+inline void KernelConf::setNrItemsD0(unsigned int items) {
+  nrItemsD0 = items;
+}
+
+inline void KernelConf::setNrItemsD1(unsigned int items) {
+  nrItemsD1;
+}
+
+inline void KernelConf::setNrItemsD2(unsigned int items) {
+  nrItemsD2;
+}
 
 } // OpenCL
 } // isa
--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -17,22 +17,30 @@
 namespace isa {
 namespace OpenCL {
 
+KernelConf::KernelConf() : nrThreadsD0(1), nrThreadsD1(1), nrThreadsD2(1), nrItemsD0(1), nrItemsD1(1), nrItemsD2(1) {}
+
+KernelConf::~KernelConf() {}
+
+std::string KernelConf::print() const {
+  return isa::utils::toString(nrThreadsD0) + " " + isa::utils::toString(nrThreadsD1) + " " + isa::utils::toString(nrThreadsD2) + " " + isa::utils::toString(nrItemsD0) + " " + isa::utils::toString(nrItemsD1) + " " + isa::utils::toString(nrItemsD2);
+}
+
 cl::Kernel * compile(const std::string & name, const std::string & code, const std::string & flags, cl::Context & clContext, cl::Device & clDevice) throw (isa::OpenCL::OpenCLError) {
   cl::Program * program = 0;
   cl::Kernel * kernel = 0;
-	try {
+  try {
     cl::Program::Sources sources(1, std::make_pair(code.c_str(), code.length()));
     program = new cl::Program(clContext, sources, NULL);
     program->build(std::vector< cl::Device >(1, clDevice), flags.c_str(), NULL, NULL);
-	} catch ( cl::Error &err ) {
-		throw isa::OpenCL::OpenCLError("Build error: " + program->getBuildInfo< CL_PROGRAM_BUILD_LOG >(clDevice));
-	}
-	try {
-		kernel = new cl::Kernel(*program, name.c_str(), NULL);
+  } catch ( cl::Error &err ) {
+    throw isa::OpenCL::OpenCLError("Build error: " + program->getBuildInfo< CL_PROGRAM_BUILD_LOG >(clDevice));
+  }
+  try {
+    kernel = new cl::Kernel(*program, name.c_str(), NULL);
     delete program;
-	} catch ( cl::Error &err ) {
-		throw isa::OpenCL::OpenCLError("Kernel error: " + name + ": " + isa::utils::toString< cl_int >(err.err()) + ".");
-	}
+  } catch ( cl::Error &err ) {
+    throw isa::OpenCL::OpenCLError("Kernel error: " + name + ": " + isa::utils::toString< cl_int >(err.err()) + ".");
+  }
 
   return kernel;
 }
